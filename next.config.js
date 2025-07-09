8fa2f77bb714e7e5556f9e66b67bf9e2cebd97c1
shisan <shisan@qq.com>
2025-07-10 00:27:27 +0800

    update task

---
+++ next.config.js
@@ -0,0 +1,31 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/posts',
+        permanent: false,
+      },
+    ];
+  },
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': __dirname,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
